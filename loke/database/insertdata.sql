ab19bbe61a4551567d8430d2f915fc8fc3da58ee
Jeppe <jepp026b@edu.zealand.dk>
2023-11-06 13:07:17 +0100

    sql fk

---
+++ loke/database/insertdata.sql
@@ -0,0 +1,3 @@
+PRAGMA foreign_keys = ON;
+INSERT INTO strategies (strategy_name, fk_exchange_id, expression)
+VALUES ('YourStrategyName', 2, 'YourExpression');
--- loke/database/trading.sql
+++ loke/database/trading.sql
@@ -1,26 +1,41 @@
 DROP TABLE IF EXISTS strategy;
 DROP TABLE IF EXISTS indicators;
 DROP TABLE IF EXISTS exchanges;
-
-CREATE TABLE strategies (
-    strategy_id INT PRIMARY KEY,
-    strategy_name VARCHAR(255) NOT NULL,
-    exchange_id INT,
-    FOREIGN KEY (exchange_id) REFERENCES exchanges(exchange_id)
+DROP TABLE IF EXISTS user;
+PRAGMA foreign_keys = ON;
+-- Create the 'user' table
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
-
--- Create the 'indicators' table
-CREATE TABLE indicators (
-    indicator_id INT PRIMARY KEY,
-    indicator_name VARCHAR(255) NOT NULL
+-- Create the 'post' table
+CREATE TABLE post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user(id)
 );
-
 -- Create the 'exchanges' table
 CREATE TABLE exchanges (
     exchange_id INT PRIMARY KEY,
     exchange_name VARCHAR(255) NOT NULL
 );
-
+CREATE TABLE strategies (
+    strategy_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    strategy_name TEXT NOT NULL,
+    fk_exchange_id INTEGER,
+    expression TEXT NOT NULL,
+    FOREIGN KEY (fk_exchange_id) REFERENCES exchanges(exchange_id)
+);
+-- Create the 'indicators' table
+CREATE TABLE indicators (
+    indicator_id INT PRIMARY KEY,
+    indicator_name VARCHAR(255) NOT NULL
+);
 -- Create the junction table 'strategy_indicators'
 CREATE TABLE strategy_indicators (
     strategy_id INT,
