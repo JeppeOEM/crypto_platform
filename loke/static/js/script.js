9a6a1e9b83abafbc81625490b228c16f99ba646f
Jeppe <jepp026b@edu.zealand.dk>
2023-11-07 14:55:11 +0100

    indi

--- loke/static/js/script.js
+++ loke/static/js/script.js
@@ -22,6 +22,53 @@ async function loadIndicator(indicatorValue, category) {
 
   // Append the newly created input field to the container
   container.appendChild(inputField);
+
+  createInputs(ind_props);
+}
+
+function createInputs(jsonData) {
+  const name_indicator = jsonData.kind;
+  const newJsonData = { ...jsonData };
+  delete newJsonData.kind;
+  const formContainer = document.getElementById("form-container");
+  const form = document.createElement("form");
+  const field = document.createElement("fieldset");
+  const legend = document.createElement("legend");
+  legend.textContent = name_indicator;
+  formContainer.appendChild(form);
+  form.appendChild(field);
+  field.appendChild(legend);
+
+  for (const key in newJsonData) {
+    const value = newJsonData[key];
+    createInput(key, value);
+  }
+
+  const submitButton = document.createElement("input");
+  submitButton.type = "submit";
+  submitButton.id = "submitButton";
+  submitButton.value = "Submit";
+  field.appendChild(submitButton);
+
+  function createInput(key, value) {
+    if (value != "bool") {
+      const label = document.createElement("label");
+      label.innerText = key;
+      const input = document.createElement("input");
+      input.type = "text";
+      input.name = key;
+      field.appendChild(label);
+      field.appendChild(input);
+    } else {
+      const label2 = document.createElement("label");
+      label2.innerText = key;
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.name = key;
+      field.appendChild(label2);
+      field.appendChild(checkbox);
+    }
+  }
 }
 
 async function postJsonString(data, endpoint) {
@@ -41,7 +88,7 @@ async function postJsonString(data, endpoint) {
     throw new Error("Request failed");
   }
 
-  const responseData = await response.text();
+  const responseData = await response.json();
 
   return responseData;
 }
